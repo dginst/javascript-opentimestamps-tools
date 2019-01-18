f6130badf6bab21348dc9763a2fa5e0ded077ab2
federicoon <gi4c0ph@gmail.com>
2019-01-18 14:43:11 +0100

    added mainnet calendars

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const OpenTimestamps = window.OpenTimestamps
 
 // an empty list would be equivalent to the default calendars
 const calendarsList = [
-    'http://test-calendar.aniasafe.it:80'
-    //'https://alice.btc.calendar.opentimestamps.org', 
+    'http://test-calendar.aniasafe.it:80',
+    'https://alice.btc.calendar.opentimestamps.org', 
     //'https://bob.btc.calendar.opentimestamps.org',
     //'https://finney.calendar.eternitywall.com'
 ]
 
 // an empty list is not acceptable here
 const wcalendars = [
-    'http://test-calendar.aniasafe.it:80'
-    //'https://alice.btc.calendar.opentimestamps.org'
+    'http://test-calendar.aniasafe.it:80',
+    'https://alice.btc.calendar.opentimestamps.org'
     //'https://bob.btc.calendar.opentimestamps.org',
     //'https://finney.calendar.eternitywall.com'
 ]
@@ -22,13 +22,15 @@ const blockexplorers = {
 	  explorers: [
     	{url: 'https://blockstream.info/api', type: 'blockstream'},
     	{url: 'https://blockexplorer.com/api', type: 'insight'}
-      ]
+      ],
+      timeout: 5
     },
     bitcoinTestnet: {
 	  explorers: [
 		{url: 'https://blockstream.info/testnet/api', type: 'blockstream'},
 		{url: 'https://testnet.blockexplorer.com/api', type: 'insight'}
-	  ]
+	  ],
+      timeout: 5
     }
 }
 
